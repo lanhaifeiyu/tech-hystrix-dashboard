ffcc4546dd857f00654f372a76b271a52e9a60a4
yurh <airson_yu@163.com>
2019-06-18 10:40:36 +0800

    init

---
+++ src/main/java/com/lhfeiyu/tech/hystrix/dashboard/TechHystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.lhfeiyu.tech.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechHystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechHystrixDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lhfeiyu/tech/hystrix/dashboard/TechHystrixDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lhfeiyu.tech.hystrix.dashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TechHystrixDashboardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
